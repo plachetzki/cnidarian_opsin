8087ab33767ce62da51192b7d9073eaa7848b408
Curtis Provencher <cao256@wildcats.unh.edu>
2018-09-25 09:36:38 -0400

    Update and rename 1_blast to 1_blast.sh

--- 1_blast.sh
+++ 1_blast.sh
@@ -2,12 +2,14 @@
 
 fasta_query=$1
 
+# iterate over the blastdb's and preform blast search with queary seqeunce file provided in first poisitional argument
 for fasta in ./blastdb/*.fas
 do
         echo "BLASTING  $fasta ..."
         blastp -query $1 -db $fasta -out ${fasta%.}_ref_blastout -outfmt 6 -max_target_seqs 50 -evalue 0.00001
 done
 
+# all out files will be in new blastout directory 
 mkdir blastout
 
 mv ./blastdb/*blastout ./blastout/
