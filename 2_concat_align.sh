8e542e425fd537de5115bbcb244c54cddcbb030b
Curtis Provencher <cao256@wildcats.unh.edu>
2018-09-26 11:52:55 -0400

    Update and rename 4_concat_align to 2_concat_align.sh

--- 2_concat_align.sh
+++ 2_concat_align.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
-# two positional arguments
+# two positional arguments, blast queary seqs and the anchor seqs (more distantly realted outgroups)
 baits=$1
 anchors=$2
 
+# remove special characters
 perl -pi -e 's/\*//g' ./hits_fasta/*.fas
 
+# run CD-HIT to remove seqs with 98% or higher seq similarity
+# add baits and anchors into fastas, and align in MAFFT
 for fasta in ./hits_fasta/*.fas 
 do
     cdhit -i $fasta -o ${fasta%.}_cdhit -c 0.98 -n 5 
@@ -13,6 +16,7 @@ do
     mafft ${fasta%.}_bts_ancrs > ${fasta%.}_ali                                                                                                                                           
 done 
 
+# new dir named align for all alignments and clean up temp files
 mkdir align
 mv ./hits_fasta/*ali ./align 
 rm ./hits_fasta/*ancrs
