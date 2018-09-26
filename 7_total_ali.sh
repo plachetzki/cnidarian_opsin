3300f3375fa0c3d0c00681f3d9ed320e0abe6ffb
Curtis Provencher <cao256@wildcats.unh.edu>
2018-09-26 12:14:06 -0400

    Update and rename 9_total_ali.sh to 7_total_ali.sh

--- 7_total_ali.sh
+++ 7_total_ali.sh
@@ -1,7 +1,10 @@
+# concatenates all fastas together making the "total tree" dataset
 mkdir concat 
 cp tip_seqs/*.fas concat/
 cp *.fa concat
 cat concat/*.fa* > concat/total_opsin.fas
 rm concat/*seqs.fas
+
+# removes any special characters and aligns total fasta in MAFFT
 perl -pi -e 's/\*//g' ./concat/total_opsin.fas
 mafft concat/total_opsin.fas > concat/total_opsin.ali
