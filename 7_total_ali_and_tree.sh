4936ce062c7c7aa0cdd3d903b286fac3fda8ba76
Curtis Provencher <cao256@wildcats.unh.edu>
2018-09-26 13:42:37 -0400

    Update and rename 7_total_ali.sh to 7_total_ali_and_tree.sh

--- 7_total_ali_and_tree.sh
+++ 7_total_ali_and_tree.sh
@@ -8,3 +8,9 @@ rm concat/*seqs.fas
 # removes any special characters and aligns total fasta in MAFFT
 perl -pi -e 's/\*//g' ./concat/total_opsin.fas
 mafft concat/total_opsin.fas > concat/total_opsin.ali
+
+# convert ali to phylip
+./seqConverter.pl -d./concat/total_opsin.ali -if -ope
+
+# Make ML tree
+raxmlHPC-PTHREADS -T 24 -s ./concat/total_opsin.phylip -m PROTGAMMAGTR -p 12345 -o ./concat/total_opsin.tre
